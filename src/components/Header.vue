2d49fab3a5f4fc7b7953a52f77714a04a9867239
KimEunJeong77 <cheritis@naver.com>
2022-01-04 16:28:06 +0900

    Start project

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header>   
+        <Logo />
+        <ul class="nav nav-pills">
+            <li 
+                class="nav-item"
+                v-for="nav in navigations"
+                :key="nav.name"
+                >
+                <RouterLink 
+                    class="nav-link" 
+                    active-class="active" 
+                    :to="nav.href">
+                    {{ nav.name }}
+                </RouterLink>
+            </li>
+        </ul>   
+    </header> 
+</template>
+<script>
+import Logo from './Logo'
+export default{
+    components:{ Logo },
+    data(){
+        return{
+            navigations:[
+                {name:'Home',href:'/'},
+                {name:'Movie',href:'/movie'},
+                {name:'About',href:'/about'}
+            ]
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+header{		
+    display:flex;	
+    align-items: center;
+    height: 70px;
+    padding: 0 40px;
+    .logo{
+        margin-right: 40px;
+    }
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </RouterLink>    
+</template>
+<style scoped lang="scss">
+@import "~/scss/main";
+.logo{
+    font-family: 'Oswald', sans-serif;
+    font-size:20px; color:$black;
+    text-decoration: none;
+    span{
+        color:$primary;
+    }
+}
+</style>
